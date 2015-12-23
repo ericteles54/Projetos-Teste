50c5a85288c4f3dce886fde63e66ee6a3a7b88b2
Eric Soluti <ericteles54@gmail.com>
2015-12-23 12:40:05 -0200

    nao sei

--- IrrigadorCasa/src/modelo/servicos/GerenciaServico.java
+++ IrrigadorCasa/src/modelo/servicos/GerenciaServico.java
@@ -31,7 +31,11 @@ public class GerenciaServico implements SystemEventListener {
 		if(event instanceof PostConstructApplicationEvent) {
 			System.out.println("Execução do método de PostConstructApplicationEvent");
 			
+			/*
+			 * Configura os pinos da GPIO na inicialização do sistema.
+			 */
 			/////////////////////////////////////////////////////////////////////////////
+			System.out.println("Configurando GPIO...");
 			this.gpio.setBomba(this.gpio.getGpio().provisionDigitalOutputPin(RaspiPin.GPIO_01, "Bomba", PinState.LOW));
 			this.gpio.setValvula(this.gpio.getGpio().provisionDigitalOutputPin(RaspiPin.GPIO_04, "Valvula", PinState.LOW));
 			
@@ -45,6 +49,14 @@ public class GerenciaServico implements SystemEventListener {
 		
 		if(event instanceof PreDestroyApplicationEvent) {
 			System.out.println("Execução do método de PreDestroyApplicationEvent");
+			
+			/*
+			 * Desliga a GPIO
+			 */
+			System.out.println("Desativando a GPIO...");
+			this.gpio.getGpio().shutdown();
+			
+			
 			this.paraServico();
 		}		
 	}
